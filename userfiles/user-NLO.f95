f7217356c4c9a47e5bedcc10ba45baa1c380edec
marialeibelt <maria.leibelt@tum.de>
2026-06-01 15:35:54 +0200

    test

--- userfiles/user-NLO.f95
+++ userfiles/user-NLO.f95
@@ -11,7 +11,7 @@
   real(kind=prec), parameter :: min_val(nrq) = (/ &
     0.295e-3_prec, 95.e3_prec, -13e-3_prec, 50._prec, -pi, &	! th3[rad], Emu[MeV], th5[rad], Eph[MeV], phi5[rad]
     -1._prec, & 						!costh3[]
-    0._prec /)!,&                                               !Qsq in MeV^2
+    1._prec /)!,&                                               !Qsq in MeV^2
     !-0.2_prec,-0.2_prec, &                                  	! x5[m], y5[m]
     !-0.2_prec,-0.2_prec,-0.2_prec,-0.2_prec,-0.2_prec,-0.2_prec,-0.2_prec,-0.2_prec,-0.2_prec,-0.2_prec, & ! x5_B1..x5_B10[m]
     !-0.2_prec,-0.2_prec,-0.2_prec,-0.2_prec,-0.2_prec,-0.2_prec,-0.2_prec,-0.2_prec,-0.2_prec,-0.2_prec /) ! y5_B1..y5_B10[m]
