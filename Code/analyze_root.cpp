952dd9c3f594af205874506a08c011ae99c6b445
marialeibelt <maria.leibelt@tum.de>
2026-08-06 18:53:07 +0200

    updated analyze.root

--- Code/analyze_root.cpp
+++ Code/analyze_root.cpp
@@ -11,9 +11,21 @@ int main()
 {
     string inputfile = "05_08_evtgen/out/05_08_evtgen.root";
 
+    cout << "Opening ROOT file: " << inputfile << endl;
+
     TFile *f = TFile::Open(inputfile.c_str());
 
+    if (!f || f->IsZombie()) {
+        cerr << "Error: could not open ROOT file!" << endl;
+        return 1;
+    }
+
     TTree *tree = (TTree*)f->Get("Output");
+
+    if (!tree) {
+        cerr << "Error: TTree 'Output' not found!" << endl;
+        return 1;
+    }
 
 
     vector<int>* scatteredPID = nullptr;
@@ -25,35 +37,12 @@ int main()
     double weight;
 
 
-    tree->SetBranchAddress(
-        "scatteredPID",
-        &scatteredPID
-    );
-
-    tree->SetBranchAddress(
-        "scatteredEnergy",
-        &scatteredEnergy
-    );
-
-    tree->SetBranchAddress(
-        "scatteredMomentumX",
-        &scatteredMomentumX
-    );
-
-    tree->SetBranchAddress(
-        "scatteredMomentumY",
-        &scatteredMomentumY
-    );
-
-    tree->SetBranchAddress(
-        "scatteredMomentumZ",
-        &scatteredMomentumZ
-    );
-
-    tree->SetBranchAddress(
-        "weight",
-        &weight
-    );
+    tree->SetBranchAddress("scatteredPID", &scatteredPID);
+    tree->SetBranchAddress("scatteredEnergy", &scatteredEnergy);
+    tree->SetBranchAddress("scatteredMomentumX", &scatteredMomentumX);
+    tree->SetBranchAddress("scatteredMomentumY", &scatteredMomentumY);
+    tree->SetBranchAddress("scatteredMomentumZ", &scatteredMomentumZ);
+    tree->SetBranchAddress("weight", &weight);
 
 
     // Plot weight distribution
@@ -61,41 +50,11 @@ int main()
 
     tree->Draw("weight");
 
-    c->SaveAs("weights.pdf");
+    string outputpdf = "weights.pdf";
+    c->SaveAs(outputpdf.c_str());
 
-
-    // Optional: print first event
-    tree->GetEntry(0);
-
-    cout << "Number of scattered particles: "
-         << scatteredPID->size()
-         << endl;
-
-
-    for(size_t i=0; i<scatteredPID->size(); i++)
-    {
-        cout << "Particle " << i << endl;
-
-        cout << " PID = "
-             << scatteredPID->at(i)
-             << endl;
-
-        cout << " E = "
-             << scatteredEnergy->at(i)
-             << endl;
-
-        cout << " px = "
-             << scatteredMomentumX->at(i)
-             << endl;
-
-        cout << " py = "
-             << scatteredMomentumY->at(i)
-             << endl;
-
-        cout << " pz = "
-             << scatteredMomentumZ->at(i)
-             << endl;
-    }
+    cout << "Weight histogram created successfully." << endl;
+    cout << "Saved as: " << outputpdf << endl;
 
 
     return 0;
